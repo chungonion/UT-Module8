4dfc1a3c2f319f4f53bf8f58cfc8ebec19e77ad4
chungonion <chungonion20@yahoo.com.hk>
2018-05-01 21:54:12 +0200

    Progress on Q19-20

--- Functional Programming/Block1/e1fp19.hs
+++ Functional Programming/Block1/e1fp19.hs
@@ -1,10 +1,12 @@
 import FPPrac.Trees
 
-data Tree1a = Leaf1a Int
-    | Node1a Int Tree1a Tree1a
-    deriving Show
+data Tree1a = Leaf1a Int| Node1a Int Tree1a Tree1a deriving Show
+data Tree1b = Leaf1b (Int,Int)| Node1b (Int,Int) Tree1b Tree1b deriving Show
 
-data Tree1b = ()
+pp1a :: Tree1a -> RoseTree
+pp1a (Leaf1a a) = RoseNode (show $ Leaf1a a) []
+pp1a (Node1a a treeA treeB) = RoseNode  (show $ Leaf1a a) ([pp1a treeA]++[pp1a treeB])
 
 pp1b :: Tree1b -> RoseTree
-pp1b tree =
+pp1b (Leaf1b a) = RoseNode (show $ Leaf1b a) []
+pp1b (Node1b a treeA treeB) = RoseNode  (show $ Leaf1b a) ([pp1b treeA]++[pp1b treeB])
---
+++ Functional Programming/Block1/e1fp20.hs
@@ -0,0 +1,14 @@
+import FPPrac.Trees
+
+data Tree1a = Leaf1a Int| Node1a Int Tree1a Tree1a deriving Show
+
+treeAdd :: Tree1a -> Int -> Tree1a
+treeAdd (Leaf1a a) x = (Leaf1a (a+x))
+treeAdd (Node1a a treeA treeB) x = (Node1a (a+x)) (treeAdd treeA x) (treeAdd treeB x)
+
+treeSquare :: Tree1a -> Tree1a
+treeSquare (Leaf1a a) = (Leaf1a (a*a))
+treeSquare (Node1a a treeA treeB) = (Node1a (a*a)) (treeSquare treeA) (treeSquare treeB)
+
+mapTree :: (Int -> Int) -> Tree1a -> Tree1a
+mapTree func 

f9c207ee6ddc7b520b58c0aa0a50e4a0d110e02f
chungonion <chungonion20@yahoo.com.hk>
2018-05-03 11:50:17 +0200

    Q19-25

--- Functional Programming/Block2/e1fp19-25.hs
+++ Functional Programming/Block2/e1fp19-25.hs
@@ -3,30 +3,54 @@ import           Data.Maybe
 import           FPPrac.Trees
 import           Test.QuickCheck
 
+--ex 19-25 All declarations
 data Tree1a = Leaf1a Int| Node1a Int Tree1a Tree1a deriving Show
 data Tree1b = Leaf1b (Int,Int)| Node1b (Int,Int) Tree1b Tree1b deriving Show
 data Tree1c = Leaf1c Int| Node1c Tree1c Tree1c deriving Show
-data Tree1d = Leaf1d (Int,Int)| Node1d Tree1d Tree1d deriving Show
+data Tree1d = Leaf1d (Int,Int)| Node1d [Tree1d] deriving Show
 data TreeInt = LeafInt | NodeInt Int TreeInt TreeInt deriving Show
+data BinTree a = Leaf | Node a (BinTree a) (BinTree a) deriving Show
 
 class BinMirror a where
     binMirror :: a -> a
 
+class PP a where
+    pp :: a -> RoseTree
+
+instance PP Tree1a where
+    pp x = pp1a x
+
+instance PP Tree1b where
+    pp x = pp1b x
+
+instance PP Tree1c where
+    pp x = pp1c x
+
+instance PP Tree1d where
+    pp x = pp1d x
+
+example1a :: Tree1a
+example1a = Node1a 2 (Node1a 3 (Leaf1a 1) (Leaf1a 1)) (Leaf1a 4)
+
+example1d :: Tree1d
+example1d = Node1d [Node1d [Node1d [Leaf1d (4,1)], Leaf1d (2,3)], Node1d [Leaf1d (3,2)], Leaf1d (1,4)]
+
+
 pp1a :: Tree1a -> RoseTree
-pp1a (Leaf1a a) = RoseNode (show $ Leaf1a a) []
+pp1a (Leaf1a a) = RoseNode (show a) []
 pp1a (Node1a a treeA treeB) = RoseNode  (show $ Leaf1a a) ([pp1a treeA]++[pp1a treeB])
 
 pp1b :: Tree1b -> RoseTree
-pp1b (Leaf1b a) = RoseNode (show $ Leaf1b a) []
+pp1b (Leaf1b a) = RoseNode (show a) []
 pp1b (Node1b a treeA treeB) = RoseNode  (show $ Leaf1b a) ([pp1b treeA]++[pp1b treeB])
 
 pp1c :: Tree1c -> RoseTree
-pp1c (Leaf1c a)           = RoseNode (show $ Leaf1c a) []
+pp1c (Leaf1c a)           = RoseNode (show a) []
 pp1c (Node1c treeA treeB) = RoseNode [] ([pp1c treeA]++[pp1c treeB])
 
 pp1d :: Tree1d -> RoseTree
-pp1d (Leaf1d a)           = RoseNode (show $ Leaf1d a) []
-pp1d (Node1d treeA treeB) = RoseNode [] ([pp1d treeA]++[pp1d treeB])
+pp1d (Leaf1d a)    = RoseNode (show a) []
+pp1d (Node1d tree) = RoseNode [] (map pp1d tree)
 
 --ex20
 
@@ -62,12 +86,11 @@ instance BinMirror Tree1d where
     binMirror x = binMirror1d x
 
 binMirror1d :: Tree1d -> Tree1d
-binMirror1d (Leaf1d a)           = (Leaf1d a)
-binMirror1d (Node1d treeA treeB) = Node1d treeB treeA
+binMirror1d (Leaf1d a)     = (Leaf1d a)
+binMirror1d (Node1d trees) = Node1d (reverse trees)
 
 
 --Ex 22
--- insertTree TO BE TESTED!!!
 insertTree :: Int -> TreeInt -> TreeInt
 insertTree input (LeafInt) = NodeInt input LeafInt LeafInt
 insertTree input (NodeInt a treeA treeB)
@@ -105,3 +128,16 @@ cutoffAt (NodeInt number treeA treeB) level
     | otherwise = (NodeInt number (cutoffAt treeA (level - 1)) (cutoffAt treeB (level - 1)))
 
 --Ex 25
+instance BinMirror (BinTree a) where
+    binMirror x = binTreeMirror x
+
+binTreeMirror ::(BinTree a)-> (BinTree a)
+binTreeMirror Leaf = Leaf
+binTreeMirror (Node a treeA treeB) = binTreeMirror (Node a treeB treeA)
+
+instance Show a =>PP (BinTree a) where
+    pp x = binTreePP x
+
+binTreePP :: Show a =>(BinTree a)-> RoseTree
+binTreePP Leaf = RoseNode "" []
+binTreePP (Node a treeA treeB) = RoseNode  (show a) ([binTreePP treeA]++[binTreePP treeB])
